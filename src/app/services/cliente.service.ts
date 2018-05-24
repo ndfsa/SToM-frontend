46a1ae5b19f8e75e6dd669deda48b94929c57925
ndfsa <25588077+ndfsa@users.noreply.github.com>
2018-05-24 15:57:22 -0400

    1

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -6,5 +6,19 @@ export class ClienteService {
 
   constructor(private http: Http) { }
 
+  getCliente(cliente) {
+    let url = 'http://localhost:8080/SToM/cliente/login';
+    let data = JSON.stringify(cliente);
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    let requestOptions = new RequestOptions({
+      method: RequestMethod.Get,
+      url: url,
+      headers: headers,
+      body: data
+    });
+    return this.http.get(url, requestOptions).forEach(res => res.json());
+  }
 
 }
