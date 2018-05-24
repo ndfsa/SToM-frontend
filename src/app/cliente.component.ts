96b386d53b329d2f85c32a5cf8158d6a07adc09e
ndfsa <sergiomarzana1@gmail.com>
2018-05-24 15:57:22 -0400

    1

--- src/app/cliente.component.ts
+++ src/app/cliente.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { ClienteService } from './services/cliente.service';
+import {Component, OnInit} from '@angular/core';
+import {ClienteService} from './services/cliente.service';
 
 @Component({
   selector: 'login-form',
   template: `
     <div class="login">
       <h1>SToM :)</h1>
-        <input type="text" [(ngModel)]="email" name="u" placeholder="E-mail" required="required" />
-        <input type="password" [(ngModel)]="password" name="u" placeholder="Password" required="required" />
-        <input type="button" value="Login!" class="btn btn-primary btn-block btn-large" />
-      </div>
+      <input type="text" [(ngModel)]="email" name="u" placeholder="E-mail" required="required"/>
+      <input type="password" [(ngModel)]="password" name="u" placeholder="Password" required="required"/>
+      <input type="button" value="Login!" class="btn btn-primary btn-block btn-large" (click)="get($event)"/>
+    </div>
   `
 })
 
@@ -17,8 +17,22 @@ export class ClienteComponent implements OnInit {
   email: any;
   password: any;
   estados: any = [];
-  constructor(private serviciod: ClienteService) { }
 
-  ngOnInit() { }
+  constructor(private serviciod: ClienteService) {
+  }
+
+  ngOnInit() {
+  }
+  get(event){
+    event.preventDefault();
+    const cliente = {
+      id_cliente: 1,
+      nombre: 'noName',
+      correo: this.email,
+      password: this.password,
+      credito: 0
+    }
+    this.serviciod.getCliente(cliente).then(estado => { this.estados.push(estado) });
+  }
 
 }
