a6aed1c9aefad22c445efd83f551fe950f573b27
ndfsa <25588077+ndfsa@users.noreply.github.com>
2018-05-24 21:52:26 -0400

    2

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -6,19 +6,32 @@ export class ClienteService {
 
   constructor(private http: Http) { }
 
-  getCliente(cliente) {
+  logCliente(cliente) {
     let url = 'http://localhost:8080/SToM/cliente/login';
     let data = JSON.stringify(cliente);
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Accept', 'application/json');
     let requestOptions = new RequestOptions({
-      method: RequestMethod.Get,
+      method: RequestMethod.Post,
       url: url,
       headers: headers,
       body: data
     });
-    return this.http.get(url, requestOptions).forEach(res => res.json());
+    return this.http.post(url, data, requestOptions).forEach(res => res.json());
   }
-
+  registerCliente(cliente){
+    let url = 'http://localhost:8080/SToM/cliente/post';
+    let data = JSON.stringify(cliente);
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    let requestOptions = new RequestOptions({
+      method: RequestMethod.Post,
+      url: url,
+      headers: headers,
+      body: data
+    });
+    return this.http.post(url, data, requestOptions).forEach(res => res.json());
+  }
 }
---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {ClienteService} from '../services/cliente.service';
+
+@Component({
+  selector: 'signup-form',
+  template: `
+    <div class="login">
+      <h1></h1>
+      <input type="text" [(ngModel)]="name" name="u" placeholder="Name" required="required"/>
+      <input type="text" [(ngModel)]="email" name="u" placeholder="E-mail" required="required"/>
+      <input type="password" [(ngModel)]="password" name="u" placeholder="Password" required="required"/>
+      <input type="button" value="Register!" class="btn btn-primary btn-block btn-large" (click)="register($event)"/>
+    </div>
+  `
+})
+export class SignupComponent implements OnInit {
+  name: any;
+  email: any;
+  password: string;
+  estados:any = [];
+  constructor(private serviciod: ClienteService) { }
+
+  ngOnInit() {
+  }
+  register(event){
+    event.preventDefault();
+    var cliente = {
+      nombre: this.name,
+      correo: this.email,
+      password: this.password
+    }
+    if(this.password.length < 8){
+      console.log("la contasena debe ser de 8 o mas caracteres");
+    }else{
+      this.serviciod.registerCliente(cliente).then(estado => { this.estados.push(estado) });
+    }
+  }
+
+}
