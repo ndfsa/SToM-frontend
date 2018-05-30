a319fabd8f5625f8d65a49fa11764d7a4cf35889
ndfsa <25588077+ndfsa@users.noreply.github.com>
2018-05-29 20:04:32 -0400

    implementar de variables globales para mantener la sesion

---
+++ src/app/globals.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class Globals {
+  public global_id_cliente: number = 0;
+  public global_nombre_cliente: string = "null";
+  public global_correo_cliente: string = "null";
+
+}
