67ea00112f0edbaa0797f748d79c34782b3b9454
ndfsa <25588077+ndfsa@users.noreply.github.com>
2018-05-31 11:16:59 -0400

    registrar cliente causa login inmediato

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ClienteService} from '../services/cliente.service';
+import {Response} from "@angular/http";
+import {Globals} from "../globals";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-signup',
@@ -10,8 +13,7 @@ export class SignupComponent implements OnInit {
   name: any;
   email: any;
   password: string;
-  estados:any = [];
-  constructor(private serviciod: ClienteService) { }
+  constructor(private serviciod: ClienteService,private globals: Globals, private router: Router) { }
 
   ngOnInit() {
   }
@@ -22,7 +24,19 @@ export class SignupComponent implements OnInit {
       correo: this.email,
       password: this.password
     }
-    this.serviciod.registerCliente(cliente).then(estado => { this.estados.push(estado) });
+    this.serviciod.registerCliente(cliente).subscribe((response:Response) => {
+      console.log(response);
+      const res = JSON.parse(response.text());
+      if (res.id_cliente != 0){
+        this.globals.global_id_cliente = res.id_cliente;
+        this.globals.global_correo_cliente = res.correo;
+        this.globals.global_nombre_cliente = res.nombre;
+        this.goToUserPage();
+      }
+    });
+  }
+  goToUserPage(){
+    return this.router.navigate(["/","userpage"])
   }
 
 }
