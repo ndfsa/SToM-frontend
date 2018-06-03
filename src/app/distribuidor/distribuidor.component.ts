0a64cb2ee4005d63c798d67609fb53809ac8ac1a
Luis Fer <pastillalc@gmail.com>
2018-06-03 19:00:15 -0400

    Componente distribuidor

---
+++ src/app/distribuidor/distribuidor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-distribuidor',
+  templateUrl: './distribuidor.component.html',
+  styleUrls: ['./distribuidor.component.css']
+})
+export class DistribuidorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
