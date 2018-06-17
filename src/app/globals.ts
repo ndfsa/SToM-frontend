d18d575237a062748699d15a0e79fc5bafff742b
x4tkc <tovilaskevin@gmail.com>
2018-06-17 17:49:08 -0400

    lista de juego modificado

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -5,10 +5,12 @@ export class Globals {
   public global_id_cliente: number = 0;
   public global_nombre_cliente: string = 'esto es un nombre';
   public global_correo_cliente: string = 'esto es un correo';
+  public global_id_juego: number = 0;
 
   signOutReset() {
     this.global_id_cliente = 0;
     this.global_nombre_cliente = 'esto es un nombre';
     this.global_correo_cliente = 'esto es un correo';
+    this.global_id_juego = 0;
   }
 }
