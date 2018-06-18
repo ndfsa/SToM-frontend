a42725ee231c43892f335b538b281005dda5ca0e
Andres <andresduarter13@gmail.com>
2018-06-17 20:12:59 -0400

    vista de juegos

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -6,11 +6,13 @@ export class Globals {
   public global_nombre_cliente: string = 'esto es un nombre';
   public global_correo_cliente: string = 'esto es un correo';
   public global_id_juego: number = 0;
-
+  public global_juegos = [];
   signOutReset() {
     this.global_id_cliente = 0;
     this.global_nombre_cliente = 'esto es un nombre';
     this.global_correo_cliente = 'esto es un correo';
     this.global_id_juego = 0;
+    this.global_juegos = [];
   }
+
 }
